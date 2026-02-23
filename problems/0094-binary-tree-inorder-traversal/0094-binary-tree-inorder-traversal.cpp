d22c118c61462f5426ee5e61dc6bf319962531b0
Satyam Kumar <128212757+unstoppable-satyam@users.noreply.github.com>
2026-02-23 08:01:09 +0000

    LeetCode Synchronization - Binary Tree Inorder Traversal (C++)

---
+++ problems/0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        if( root == nullptr ) return {};
+        stack<TreeNode* > st;
+        vector<int> ret;
+        TreeNode* curr = root;
+        while(curr || !st.empty()){
+            while(curr != nullptr){
+                st.push(curr);
+                curr = curr->left;
+                
+            }
+            curr = st.top();
+            st.pop();
+            ret.push_back(curr->val);
+            curr = curr->right;
+        }
+        return ret;
+    }
+};
